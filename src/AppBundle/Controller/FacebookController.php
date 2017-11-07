d741ca1ac617d63c940a51a5bb0c2c0193cc9f14
Matas Dominas <mdominas@gmail.com>
2017-11-07 14:20:15 +0200

    User name display

--- src/AppBundle/Controller/FacebookController.php
+++ src/AppBundle/Controller/FacebookController.php
@@ -25,7 +25,7 @@ class FacebookController extends Controller
         $user = $this->get('security.token_storage')->getToken()->getUser();
 
 //        return $this->generateUrl('test');
-        return $this->render('AppBundle:Home:user.html.twig', [
+        return $this->redirectToRoute('home', [
             'user' => $user
         ]);
     }
