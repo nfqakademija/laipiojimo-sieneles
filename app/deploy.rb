4530d70321f2c0dafd5e0ff57d48d09b003aeb73
Matas Dominas <mdominas@gmail.com>
2017-11-08 23:58:51 +0200

    Deploy fix

--- app/deploy.rb
+++ app/deploy.rb
@@ -9,4 +9,4 @@ set :scm, :rsync
 set :exclude, ['.git', 'node_modules', 'web/app_dev.php']
 
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push(%w{var/logs web/uploads})
+set :linked_dirs, fetch(:linked_dirs, []).push('var/logs')
