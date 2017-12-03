9295739cab4025fae9532e06a0345a23553e492a
Matas Dominas <mdominas@gmail.com>
2017-12-03 20:40:08 +0200

    Controller route annotation fix

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -8,11 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class HomeController extends Controller
 {
     /**
-     * Class HomeController
-     *
-     * @package AppBundle\Controller
-     *
-     * @Route("/{_locale}", defaults={"_locale": "lt"}, requirements={"_locale" = "%app.locales%"})
+     * @Route("", name="home")
      */
     public function indexAction()
     {
